d9d311e26e20c9e43ee484d2f9237d837c05106d
Karen Aiwazian <karenaiwazian@outlook.com>
2026-08-03 15:50:57 +0300

    feat(admins): эндпоинты администраторов и проверка прав в каналах и группах

--- src/modules/channels/channels.module.ts
+++ src/modules/channels/channels.module.ts
@@ -10,16 +10,19 @@ import { EncryptionService } from '../encryption/encryption.service'
 import { InviteLinksService } from '../invites/invite-links.service'
 import { CreateChannelUseCase } from './use-cases/create-channel.use-case'
 import { StorageService } from '../storage/storage.service'
+import { ChannelAdminsService } from './channel-admins.service'
 
 @Module({
 	imports: [JwtAuthModule, SessionsModule, SearchModule, ChatsModule, PrismaModule],
 	controllers: [ChannelsController],
 	providers: [
 		ChannelsService,
+		ChannelAdminsService,
 		EncryptionService,
 		InviteLinksService,
 		CreateChannelUseCase,
 		StorageService
-	]
+	],
+	exports: [ChannelAdminsService]
 })
 export class ChannelsModule {}
